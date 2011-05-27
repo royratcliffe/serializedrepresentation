ec8ff8d49fecf84f0aab1f29777d5f60e931a94c
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2011-05-27 13:01:54 +0100

    Initial commit, command-line tool using Xcode 4.0.2

---
+++ serializedrepresentation/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  serializedrepresentation
+//
+//  Created by Roy Ratcliffe on 27/05/2011.
+//  Copyright 2011 Pioneering Software, United Kingdom. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+	NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+	// insert code here...
+	NSLog(@"Hello, World!");
+
+	[pool drain];
+    return 0;
+}
+
